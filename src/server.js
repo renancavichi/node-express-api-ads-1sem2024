1b4efd5138fc9d3fe3c91574801404734ae1052a
Renan Cavichi <renancavichi@gmail.com>
2024-02-22 19:58:46 -0300

    add crud routes

--- src/server.js
+++ src/server.js
@@ -5,10 +5,12 @@ import {users} from './db-memory/user.js'
 
 const app = express()
 
+//middleware
 app.use(express.json())
 
+//routes
 app.get('/', (req, res) => {
-  res.json({message: "Bem-vindo a API!"})
+  res.json({success: "Bem-vindo a API!"})
 })
 
 app.get('/user', (req, res) => {
@@ -23,12 +25,41 @@ app.post('/user', (req, res) => {
   user.id = users[users.length - 1].id + 1 
   users.push(user)
   res.json({
-    success: "Usuários listados com sucesso!",
+    success: "Usuários adicionado com sucesso!",
     users
   })
 })
 
+app.delete('/user', (req, res) => {
+  const id = req.body.id
+  const usersResult = users.filter(user => user.id !== id)
+  res.json({
+    success: `Usuário ${id} removido com sucesso!`,
+    usersResult
+  })
+})
 
+app.put('/user', (req, res) => {
+  const newUser = req.body
+  const usersResult = users.map(user => {
+    if(user.id === newUser.id){
+      return {
+        id: user.id,
+        name: newUser.name || user.name,
+        email: newUser.email || user.email,
+        avatar: newUser.avatar || user.avatar
+      }
+    }
+    return user
+  })
+
+  res.json({
+    success: `Usuário ${newUser.id} atualizado com sucesso!`,
+    usersResult
+  })
+})
+
+//run server
 app.listen(PORT, () => {
   console.log(`Servidor rodando em ${HOST}:${PORT}`)
 })
