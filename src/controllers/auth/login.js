8647d6ab0d597aacf1a0e6e0876efab58f16a693
Renan Cavichi <renancavichi@gmail.com>
2024-04-25 21:52:43 -0300

    add try catch, generate refresh token cookie, get user agent

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -7,51 +7,58 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const login = async (req, res) => {
-    //TODO: lembrar try catch
-    const {email, pass} = req.body
+    try{
+        const {email, pass} = req.body
 
-    //pegar pass (hash) buscando pelo email 
-    const userFound = await userModel.getByEmail(email)
-    if(!userFound) return res.status(401).json({
-        error: "Email ou senha inválida!"
-    })
-    //verificar se o hash é válido (bcrypt)
-    const isValid = await compare(pass, userFound.pass)
-    if(!isValid) return res.status(401).json({
-        error: "Email ou senha inválida!"
-    })
+        //pegar pass (hash) buscando pelo email 
+        const userFound = await userModel.getByEmail(email)
+        if(!userFound) return res.status(401).json({
+            error: "Email ou senha inválida!"
+        })
+        //verificar se o hash é válido (bcrypt)
+        const isValid = await compare(pass, userFound.pass)
+        if(!isValid) return res.status(401).json({
+            error: "Email ou senha inválida!"
+        })
 
-    //continuar o login e gerar os token de acesso
-    const accessToken = jwt.sign(
-        {id: userFound.id, name: userFound.name}, //payload - dados que você quer guardar no token
-        SECRET_KEY,  //Chave secreta
-        {expiresIn: '1m'}
-        )
-    
-    const refreshToken = jwt.sign(
-        {id: userFound.id}, //payload - dados que você quer guardar no token
-        SECRET_KEY,  //Chave secreta
-        {expiresIn: '3m'}
-        )
+        //continuar o login e gerar os token de acesso
+        const accessToken = jwt.sign(
+            {id: userFound.id, name: userFound.name}, //payload - dados que você quer guardar no token
+            SECRET_KEY,  //Chave secreta
+            {expiresIn: '1m'}
+            )
+        
+        const refreshToken = jwt.sign(
+            {id: userFound.id}, //payload - dados que você quer guardar no token
+            SECRET_KEY,  //Chave secreta
+            {expiresIn: '3m'}
+            )
 
-    //TODO: gerar cookie para web
-    await prisma.session.create({
-        data: {
-            user_id: userFound.id,
-            //TODO: pegar header User-Agent
-            client: "API DOG",
-            token: refreshToken
-        }
-    })
+        await prisma.session.create({
+            data: {
+                user_id: userFound.id,
+                client: req.headers['user-agent'],
+                token: refreshToken
+            }
+        })
 
-    delete userFound.pass
-    return res.json({
-		success: `Usuário do login!`,
-		user: userFound,
-        accessToken,
-        refreshToken
-	})
+        delete userFound.pass
 
+        //gera o cookie do refresh token para web (3 meses) 3 * 30 * 24 * 60 * 60 * 1000
+        res.cookie('refreshToken', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 3 * 30 * 24 * 60 * 60 * 1000 })
+
+        return res.json({
+            success: `Usuário do login!`,
+            user: userFound,
+            accessToken,
+            refreshToken
+        })
+    } catch(error){
+        console.log(error)
+        return res.status(500).json({
+            error: "Erro interno, tente novamente mais tarde!"
+        })
+    }
 }
 
 export default login
