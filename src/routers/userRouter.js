64e716b68158d2b85fe2a3005ad28ca6f6839167
Renan Cavichi <renancavichi@gmail.com>
2024-05-23 21:44:32 -0300

    add auth user routes

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,12 +4,14 @@ import create from '../controllers/user/create.js'
 import remove from '../controllers/user/remove.js'
 import edit from '../controllers/user/edit.js'
 import getById from '../controllers/user/getById.js'
+import auth from '../middlewares/auth.js'
 
 const router = express.Router()
 
 router.get('/', listAll)
-router.get('/:id', getById)
 router.post('/', create)
+router.use(auth)
+router.get('/:id', getById)
 router.delete('/:id', remove)
 router.put('/:id', edit)
 
